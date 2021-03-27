6b98203c69886b09f977a6f6bd049f3b9688fee7
Janemasha <janemasharo@gmail.com>
2021-03-27 23:19:36 +0300

    add task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let arr = [];
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i ++) {
+      if(typeof(members[i]) == "string") {
+        for (let j = 0; j < members[i].length; j ++) {
+          if (members[i][j] != " ") {
+          arr.push(members[i][j].toUpperCase());
+          break;
+        }
+        }
+      }
+    }
+    if (arr.length == 0) {
+      return false
+    } else {
+      arr.sort();
+    return arr.join('');
+    }
+  } else {
+    return false
+    }
+}
