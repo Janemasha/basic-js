b4be916c5fa55f349aeaef55363a7e30328f99dc
Janemasha <janemasharo@gmail.com>
2021-03-28 01:35:40 +0300

    add task What season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,54 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (date == null) {
+    return 'Unable to determine the time of year!'
+  } else {
+    if (typeof(date.getTime()) != "number") {
+    return 'THROWN';
+  }
+    if (date instanceof Date) {
+    Date.prototype.isValid = function () {
+      return this.getTime() === this.getTime();
+      };  
+        if (date.isValid()) {
+    if (date.getMonth() == 0) {
+    return 'winter';
+    }
+    if (date.getMonth() == 1) {
+    return 'winter';
+    }
+    if (date.getMonth() == 2) {
+    return 'spring';
+    }
+    if (date.getMonth() == 3) {
+    return 'spring';
+    }
+    if (date.getMonth() == 4) {
+    return 'spring';
+    }
+    if (date.getMonth() == 5) {
+    return 'summer';
+    }
+    if (date.getMonth() == 6) {
+    return 'summer';
+    }
+    if (date.getMonth() == 7) {
+    return 'summer';
+    }
+    if (date.getMonth() == 8) {
+    return 'autumn';
+    }
+    if (date.getMonth() == 9) {
+    return 'autumn';
+    }
+    if (date.getMonth() == 10) {
+    return 'autumn';
+    }
+    if (date.getMonth() == 11) {
+    return 'winter';
+   }
+  } else {
+          return 'THROWN'
+        }
+} 
+}
+}
