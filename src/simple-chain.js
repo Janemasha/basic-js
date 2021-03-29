40caea648bb21c493cec1c7575e381a59e62b8e9
Janemasha <janemasharo@gmail.com>
2021-03-30 02:47:37 +0300

    add task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,5 +1,4 @@
 const chainMaker = {
-  val: "",
   getLength() {
     if (this.val == "") {
       return 0;
@@ -9,7 +8,8 @@ const chainMaker = {
     }
   },
   addLink(value) {
-    if (this.val == "") {
+    value = String(value);
+    if (this.val == null) {
       this.val = `( ${value} )`;
     } else {
       let arr = this.val.split("~~");
@@ -19,22 +19,29 @@ const chainMaker = {
     return this;
   },
   removeLink(position) {
-    if (position > 0 && position < this.val.length && Number.isInteger(position)) {
+    if (this.val != null) {
+    let arr = this.val.split("~~");
+    if (position > 0 && position < arr.length && isFinite(position)) {
     if (this.val != "") {
-      let arr = this.val.split("~~");
       arr.splice(position - 1, 1);
       this.val = arr.join("~~");
-    }}
+    }} else {
+      this.val = null;
+    }
     return this;
-  },
+  }},
   reverseChain() {
+    if (this.val != null && this.val != undefined) {
     let arr = this.val.split("~~");
     arr = arr.reverse();
     this.val = arr.join("~~");
     return this;
+  }
   },
   finishChain() {
-    return this.val;
+    let srt = this.val;
+    this.val = null;
+    return srt;
   }
 };
 
